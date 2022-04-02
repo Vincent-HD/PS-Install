a568f0da1b6cb9651ea475998b44386977b97a68
Vincent <vincenthoudan@gmail.com>
2022-04-02 20:59:10 +0200

    Add output steps

--- install.ps1
+++ install.ps1
@@ -15,6 +15,8 @@ $install_list.ForEach({
     # Test if file exist (Leaf = is a file)
     if (Test-Path -Path "$scripts_path/$($_.Filename).ps1" -PathType Leaf) {
         # Include file
+        Write-Output "Exécution du script $($_.Name) : $scripts_path/$($_.Filename).ps1"
         Invoke-Expression -Command "$scripts_path/$($_.Filename).ps1"
+        Write-Output "Fin de l'éxécution du script"
     }
 })
--- scripts/mpv.ps1
+++ scripts/mpv.ps1
@@ -3,15 +3,28 @@ function Install {
     $ytdlp = "yt-dlp"
     $python3 = "Python.Python.3"
     $script_path = "$env:APPDATA/mpv.net/scripts"
+    $repo_sponsorblock = "https://github.com/po5/mpv_sponsorblock.git"
 
+    Write-Output "Installation de MPV - ID: $mpv_id"
     Install-WingetPackage $mpv_id
+    Write-Output "Installation terminée"
+    Write-Output "Installation de ytdlp"
     Install-ChocoPackage $ytdlp
+    Write-Output "Installation terminée"
+    Write-Output "Installation de Python3 - ID: $python3"
     Install-WingetPackage $python3
-
+    Write-Output "Installation terminée"
+    
     if (-Not(Test-Path -Path $script_path -PathType Container )) {
+        Write-Output "Dossier manquant, création du dossier: $script_path"
         New-Item -Path $script_path -ItemType Directory
     }
+    Write-Output "Téléchargement du fichier de configuration dans: $script_path/youtube-quality.lua"
     Invoke-WebRequest -Uri "https://pastebin.com/raw/TqzZLUrD" -OutFile "$script_path/youtube-quality.lua"
-    global:Invoke-GitClone "https://github.com/po5/mpv_sponsorblock.git" "$env:APPDATA/mpv.net/scripts"
+    Write-Output "Téléchargement terminé"
+
+    Write-Output "Clone du repo: $repo_sponsorblock"
+    global:Invoke-GitClone $repo_sponsorblock "$env:APPDATA/mpv.net/scripts"
+    Write-Output "Clone terminé"
 }
 Install
