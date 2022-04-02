29e5add8437672d171a930a1e0361085ca7f3e75
Vincent <vincenthoudan@gmail.com>
2022-04-02 21:46:26 +0200

    Fix output color

--- install.ps1
+++ install.ps1
@@ -16,8 +16,8 @@ $install_list.ForEach({
     # Test if file exist (Leaf = is a file)
     if (Test-Path -Path "$scripts_path/$($_.Filename).ps1" -PathType Leaf) {
         # Include file
-        Write-Output -ForegroundColor Yellow "Exécution du script $($_.Name) : $scripts_path/$($_.Filename).ps1"
+        Write-Host -ForegroundColor Yellow "Exécution du script $($_.Name) : $scripts_path/$($_.Filename).ps1"
         Invoke-Expression -Command "$scripts_path/$($_.Filename).ps1"
-        Write-Output -ForegroundColor Yellow "Fin de l'éxécution du script"
+        Write-Host -ForegroundColor Yellow "Fin de l'éxécution du script"
     }
 })
--- scripts/mpv.ps1
+++ scripts/mpv.ps1
@@ -5,26 +5,26 @@ function Install {
     $script_path = "$env:APPDATA/mpv.net/scripts"
     $repo_sponsorblock = "https://github.com/po5/mpv_sponsorblock.git"
 
-    Write-Output -ForegroundColor Yellow "Installation de MPV - ID: $mpv_id"
+    Write-Host -ForegroundColor Yellow "Installation de MPV - ID: $mpv_id"
     Install-WingetPackage $mpv_id
-    Write-Output -ForegroundColor Yellow "Installation terminée"
-    Write-Output -ForegroundColor Yellow "Installation de ytdlp"
+    Write-Host -ForegroundColor Yellow "Installation terminée"
+    Write-Host -ForegroundColor Yellow "Installation de ytdlp"
     Install-ChocoPackage $ytdlp
-    Write-Output -ForegroundColor Yellow "Installation terminée"
-    Write-Output -ForegroundColor Yellow "Installation de Python3 - ID: $python3"
+    Write-Host -ForegroundColor Yellow "Installation terminée"
+    Write-Host -ForegroundColor Yellow "Installation de Python3 - ID: $python3"
     Install-WingetPackage $python3
-    Write-Output -ForegroundColor Yellow "Installation terminée"
+    Write-Host -ForegroundColor Yellow "Installation terminée"
     
     if (-Not(Test-Path -Path $script_path -PathType Container )) {
-        Write-Output -ForegroundColor Yellow "Dossier manquant, création du dossier: $script_path"
+        Write-Host -ForegroundColor Yellow "Dossier manquant, création du dossier: $script_path"
         New-Item -Path $script_path -ItemType Directory
     }
-    Write-Output -ForegroundColor Yellow "Téléchargement du fichier de configuration dans: $script_path/youtube-quality.lua"
+    Write-Host -ForegroundColor Yellow "Téléchargement du fichier de configuration dans: $script_path/youtube-quality.lua"
     Invoke-WebRequest -Uri "https://pastebin.com/raw/TqzZLUrD" -OutFile "$script_path/youtube-quality.lua"
-    Write-Output -ForegroundColor Yellow "Téléchargement terminé"
+    Write-Host -ForegroundColor Yellow "Téléchargement terminé"
 
-    Write-Output -ForegroundColor Yellow "Clone du repo: $repo_sponsorblock"
+    Write-Host -ForegroundColor Yellow "Clone du repo: $repo_sponsorblock"
     global:Invoke-GitClone $repo_sponsorblock "$env:APPDATA/mpv.net/scripts"
-    Write-Output -ForegroundColor Yellow "Clone terminé"
+    Write-Host -ForegroundColor Yellow "Clone terminé"
 }
 Install
