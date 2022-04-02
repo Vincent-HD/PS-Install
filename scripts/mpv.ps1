522683b02d417fec08adba22a83f9c277d34c40b
Vincent <vincenthoudan@gmail.com>
2022-04-02 23:56:12 +0200

    Add config files

--- scripts/mpv.ps1
+++ scripts/mpv.ps1
@@ -2,7 +2,8 @@
     $mpv_id = "5664FrankSkare.mpv.net_0ta2kwwjvkgap"
     $ytdlp = "yt-dlp"
     $python3 = "Python.Python.3"
-    $script_path = "$env:APPDATA/mpv.net/scripts"
+    $mpv_path = "$env:APPDATA/mpv.net"
+    $script_path = "$mpv_path/scripts"
     $repo_sponsorblock = "https://github.com/po5/mpv_sponsorblock.git"
 
     Install-WingetPackage $mpv_id
@@ -20,5 +21,7 @@
     Write-Host -ForegroundColor Yellow "Clone du repo: $repo_sponsorblock"
     global:Invoke-GitClone $repo_sponsorblock "$env:APPDATA/mpv.net/scripts"
     Write-Host -ForegroundColor Yellow "Clone terminé"
+    Write-Host -ForegroundColor Yellow "Copie des fichiers de configuration"
+    Move-Item -Path "../conf/mpv/*" -Destination $mpv_path -Force
 }
 Install
