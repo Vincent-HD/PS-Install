669cef92b088d6e00d7afb9cc16f50491f18300a
Vincent <vincenthoudan@gmail.com>
2022-04-02 21:19:37 +0200

    Output color

--- scripts/mpv.ps1
+++ scripts/mpv.ps1
@@ -5,26 +5,26 @@ function Install {
     $script_path = "$env:APPDATA/mpv.net/scripts"
     $repo_sponsorblock = "https://github.com/po5/mpv_sponsorblock.git"
 
-    Write-Output "Installation de MPV - ID: $mpv_id"
+    Write-Output -ForegroundColor Yellow "Installation de MPV - ID: $mpv_id"
     Install-WingetPackage $mpv_id
-    Write-Output "Installation terminée"
-    Write-Output "Installation de ytdlp"
+    Write-Output -ForegroundColor Yellow "Installation terminée"
+    Write-Output -ForegroundColor Yellow "Installation de ytdlp"
     Install-ChocoPackage $ytdlp
-    Write-Output "Installation terminée"
-    Write-Output "Installation de Python3 - ID: $python3"
+    Write-Output -ForegroundColor Yellow "Installation terminée"
+    Write-Output -ForegroundColor Yellow "Installation de Python3 - ID: $python3"
     Install-WingetPackage $python3
-    Write-Output "Installation terminée"
+    Write-Output -ForegroundColor Yellow "Installation terminée"
     
     if (-Not(Test-Path -Path $script_path -PathType Container )) {
-        Write-Output "Dossier manquant, création du dossier: $script_path"
+        Write-Output -ForegroundColor Yellow "Dossier manquant, création du dossier: $script_path"
         New-Item -Path $script_path -ItemType Directory
     }
-    Write-Output "Téléchargement du fichier de configuration dans: $script_path/youtube-quality.lua"
+    Write-Output -ForegroundColor Yellow "Téléchargement du fichier de configuration dans: $script_path/youtube-quality.lua"
     Invoke-WebRequest -Uri "https://pastebin.com/raw/TqzZLUrD" -OutFile "$script_path/youtube-quality.lua"
-    Write-Output "Téléchargement terminé"
+    Write-Output -ForegroundColor Yellow "Téléchargement terminé"
 
-    Write-Output "Clone du repo: $repo_sponsorblock"
+    Write-Output -ForegroundColor Yellow "Clone du repo: $repo_sponsorblock"
     global:Invoke-GitClone $repo_sponsorblock "$env:APPDATA/mpv.net/scripts"
-    Write-Output "Clone terminé"
+    Write-Output -ForegroundColor Yellow "Clone terminé"
 }
 Install
